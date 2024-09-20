d5161783e471d975006433d7c60400edffb67e07
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-09-20 10:42:11 -0400

    feat: add first rote to create users

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -7,6 +7,8 @@ import {
   ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 
+import { createAccount } from './routes/auth/create-account'
+
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 /* Processo de transformação de inputs e outputs */
@@ -17,6 +19,10 @@ app.setValidatorCompiler(validatorCompiler)
 
 /* Permitir qq front end acessar */
 app.register(fastifyCors)
+
+/* Rotas */
+
+app.register(createAccount)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!')
