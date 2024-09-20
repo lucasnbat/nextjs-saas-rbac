9c9f46e7a24a6538345bc071c1eadf5caa5e9e3b
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-09-20 10:30:57 -0400

    feat: finish `server.ts` with the serializer and validator

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,13 +1,19 @@
 import fastifyCors from '@fastify/cors'
 import { fastify } from 'fastify'
 import {
-  jsonSchemaTransform,
+  // jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+/* Processo de transformação de inputs e outputs */
+app.setSerializerCompiler(serializerCompiler)
+
+/* Validação com validador vindo da lib do zod */
+app.setValidatorCompiler(validatorCompiler)
 
 /* Permitir qq front end acessar */
 app.register(fastifyCors)
