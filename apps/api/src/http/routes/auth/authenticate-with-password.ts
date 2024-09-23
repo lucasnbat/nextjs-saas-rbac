d0a72e559984f697e12edc7197f1900d311f45f3
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-09-23 11:17:15 -0400

    feat: add new method of authentication

---
+++ apps/api/src/http/routes/auth/authenticate-with-password.ts
@@ -0,0 +1,20 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+export async function authenticateWithPassword(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/sessions/password',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Authenticate with e-mail & password',
+        body: z.object({
+          email: z.string().email(),
+          password: z.string(),
+        }),
+      },
+    },
+    async (request, reply) => {},
+  )
+}
